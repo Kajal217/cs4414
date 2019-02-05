e29af88e8a56cda2e465233665c79ea8254c8a56
AustinTB <baneyat@gmail.com>
2019-02-04 23:46:55 -0500

    kill

--- shell/main.cc
+++ shell/main.cc
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <string>
 #include <stdio.h>
@@ -6,29 +7,20 @@
 #include <sstream>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
 
-class command {
- public:
-  std::string cmd;
-  std::vector<std::string> args;
+typedef struct
+{
+  const char* args[20];
+  std::string name;
+} command_t;
 
-  command() {
-    cmd = "";
-    args = std::vector<std::string>(0);
-  }
-  
-  void addArg(std::string arg) {
-    args.push_back(arg);
-  }
-  //return a c-style array of args
-  const char* argArray() {
-    const char* arr[10];
-    for (uint i=0; i<args.size(); i++) {
-      arr[i] = args[i].c_str();
-    }
-    return *arr;
-  }
-};
+int isOperator(char* token) {
+  if (strlen(token) > 1) return 0;
+  if (*token == '>' || *token == '<') return 1;
+  return 0;
+}
 
 void parse_and_run_command(const std::string &cmd) {
     /* TODO: Implement this. */
@@ -42,33 +34,66 @@ void parse_and_run_command(const std::string &cmd) {
 
   //create commands (deal with operators later)
   //pipeline object?
-  command *cur = new command();
+  int numOps = 0;
+  std::vector<const char*> opArr;
+  command_t c;
   for (uint i=0; i < tokens.size(); i++) {
-    if (cur->cmd == "") cur->cmd = tokens[i];
-    //if operator...
-    else cur->addArg(tokens[i]);
+    if (tokens[i] == "&" && i != tokens.size()-1) {
+	  std::cerr << "Invalid command.\n";
+	  exit(1);
+	}
+    if (isOperator((char*)tokens[i].c_str()) == 1) {
+	numOps++;
+	opArr.push_back(tokens[i].c_str());
+    }
+    if (i==0){
+      c.name = tokens[i].c_str();
+      c.args[0] = "";
+    }
+    else c.args[i-1] = tokens[i].c_str();
   }
 
+  //ERROR HANDLING
+
+  // missing file
+  if (c.name != "exit" && isOperator((char*)c.name.c_str()) == 0) {  
+    DIR* dir = opendir(c.name.c_str());
+    if (dir) {
+      closedir(dir);
+    }
+    else if (ENOENT == errno) {
+    std::cerr << "No such file or directory.\n";
+    exit(1);
+    }
+  }
+
+  //
+  if (numOps > 2) {
+    std::cerr << "Invalid command.\n";
+    exit(1);
+  }
+  
   //for each command in the line
-  std::vector<int> pids;
+  std::vector<pid_t> pids;
+  pid_t pid;
   int status;
   for (uint j = 0; j < 1; j++) {
-    if (cur->cmd == "exit") exit(0);
-    pid_t pid = fork();
+    if (c.name == "exit") exit(0);
+    pid = fork();
     if (pid==0) {
       //redirection stuff here*
-      execv(cur->cmd.c_str(), (char**)cur->argArray());
-      std::cerr << "exec failed, exiting...";
-      exit(0);
+      execv(c.name.c_str(), (char**)c.args);
+      std::cerr << "exec failed, exiting...\n";
+      exit(1);
     }
     else pids.push_back(pid);
   }
   //for each command in the line
+  
   for (uint k=0; k<pids.size(); k++) {
     waitpid(pids[k], &status, 0);
-    printf("%s exited with status: %d", cur->cmd.c_str(), status);
+    printf("%s exit status: %d.\n", c.name.c_str(), status/256);
   }
-
 }
 
 int main(void) {
