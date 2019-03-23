d681d5f34c3a8e308d59cbb4f8117817bbfc0f7b
austin <baneyat@gmail.com>
2019-03-22 21:18:07 -0400

    dont panic

--- paging/xv6/vm.c
+++ paging/xv6/vm.c
@@ -324,11 +324,11 @@ copyuvm(pde_t *pgdir, uint sz) // for checkpoint: should only copy pages that we
     return 0;
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0){
-      panic("copyuvm: pte should exist");
+      //panic("copyuvm: pte should exist");
       continue;
     }
     if(!(*pte & PTE_P)){
-      panic("copyuvm: page not present");
+      //panic("copyuvm: page not present");
       continue;
     }
     pa = PTE_ADDR(*pte);
