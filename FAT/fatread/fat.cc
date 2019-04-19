ccaba3c3f80e4fa2acb46cea9b7fad7ffbe9e9d6
austin <baneyat@gmail.com>
2019-04-19 06:58:50 -0400

    back to 0 instead of null term

--- FAT/fatread/fat.cc
+++ FAT/fatread/fat.cc
@@ -159,7 +159,7 @@ DirEntry* getDirs(DirEntry* dir, uint32_t* sizePtr) {
   currEnt = myEntries;
   uint32_t i = 0; // was int
   //count dirs
-  while(currEnt->DIR_Name[0] != '\0'){  // null term or just 0?? check i < *numEntries??
+  while(currEnt->DIR_Name[0] != 0){  // null term or just 0?? check i < *numEntries??
     if (currEnt->DIR_Attr & DirEntryAttributes::DIRECTORY || currEnt->DIR_Attr & DirEntryAttributes::VOLUME_ID) 
       numDirs++;
     i++;
@@ -173,7 +173,7 @@ DirEntry* getDirs(DirEntry* dir, uint32_t* sizePtr) {
   currEnt = myEntries;
   i=0;
   //find and copy dirs into new array
-  while(currEnt->DIR_Name[0] != '\0'){  // ^^
+  while(currEnt->DIR_Name[0] != 0){  // ^^
     if (currEnt->DIR_Attr & DirEntryAttributes::DIRECTORY || currEnt->DIR_Attr & DirEntryAttributes::VOLUME_ID){
       memcpy(&(myDirs[dirIndex]),currEnt,sizeof(DirEntry));
       dirIndex++;
