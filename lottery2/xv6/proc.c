5b7b7045fbcbba1c2f2b625b07d8582bed5e42ef
austin <baneyat@gmail.com>
2019-09-26 17:46:51 -0400

    increment ticks in scheduler

--- lottery2/xv6/proc.c
+++ lottery2/xv6/proc.c
@@ -344,6 +344,7 @@ scheduler(void)
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
+      p->ticks++;
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
