5c044d251cd0ffec988f9fb8184351d7c63d069f
austin <baneyat@gmail.com>
2019-10-03 20:16:01 -0400

    doesnt count

--- life2/life-parallel.cc
+++ life2/life-parallel.cc
@@ -2,6 +2,7 @@
 // Based on my own solution from S19
 #include "life.h"
 #include <pthread.h>
+#include <sys/wait.h>
 
 struct thread_args {
   LifeBoard* state;
@@ -26,7 +27,7 @@ void simulate_life_parallel(int threads, LifeBoard &state, int steps) {
     // fill arguments struct for each thread
     args[0].start = 0;
     args[0].end = cellsPerThread;
-    for (int i = 0; i < threads, i++) {
+    for (int i = 0; i < threads; i++) {
         args[i].state = &state;
         args[i].next_state = &next_state;
         args[i].barrier_ptr = &barrier;
@@ -64,18 +65,18 @@ void* run_thread(void* arguments) {
     LifeBoard* next_state = args->next_state;
     pthread_barrier_t* barrier_ptr = args->barrier_ptr;
     int steps = args->steps;
-    int start = args->startIndex;
-    int end = args->endIndex;
+    int start = args->start;
+    int end = args->end;
 
     for (int step = 0; step < steps; ++step) {
         /* We use the range [1, width - 1) here instead of
          * [0, width) because we fix the edges to be all 0s.
          */
         int index = 0;
-        for (int y = 1; y < state.height() - 1; ++y) {
-            for (int x = 1; x < state.width() - 1; ++x) {
+        for (int y = 1; y < state->height() - 1; ++y) {
+            for (int x = 1; x < state->width() - 1; ++x) {
                 // only operate on the specified cells for this thread
-                if (index < startIndex) {
+                if (index < start) {
                     index++;
                     continue;
                 }
@@ -86,21 +87,21 @@ void* run_thread(void* arguments) {
                  * and count the number of live (true) cells in the window. */
                 for (int y_offset = -1; y_offset <= 1; ++y_offset) {
                     for (int x_offset = -1; x_offset <= 1; ++x_offset) {
-                        if (state.at(x + x_offset, y + y_offset)) {
+                        if (state->at(x + x_offset, y + y_offset)) {
                             ++live_in_window;
                         }
                     }
                 }
                 /* Cells with 3 live neighbors remain or become live.
                    Live cells with 2 live neighbors remain live. */
-                next_state.at(x, y) = (
+                next_state->at(x, y) = (
                     live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
-                    (live_in_window == 4 && state.at(x, y)) /* live cell with 3 neighbors */
+                    (live_in_window == 4 && state->at(x, y)) /* live cell with 3 neighbors */
                 );
 
                 index++;
             }
-            if (index == endIndex) break;
+            if (index == end) break;
         }
         
         // wait for other threads to finish this generation
