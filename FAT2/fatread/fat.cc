7bb6b4de049dacf52be1b4a38510c59154fb9071
austin <baneyat@gmail.com>
2019-11-14 20:14:32 -0500

    .

--- FAT2/fatread/fat.cc
+++ FAT2/fatread/fat.cc
@@ -1,12 +1,18 @@
 // Austin Baney // ab5ep
 // Based on my S19 implementation
 #include "fat_internal.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <conio.h>
 #include <string.h>
 
 
 int Disk = -1;  // the FAT disk image
 Fat32BPB BPB;   // the header with info like the locations of FAT and root directory
-uint32_t FAT;   // the File Allocation Table - stores next cluster number
+uint32_t* FAT;   // the File Allocation Table - stores next cluster number
 DirEntry* FileTable[128];   // tracks open files, indexed by FDs
 uint32_t FirstDataSector, DataOffset, ClusterSize, EntsPerCluster;
 
@@ -48,15 +54,19 @@ DirEntry* readClusterChain(uint32_t clusterNum, uint32_t* sizePtr) {
 
 bool fat_mount(const std::string &path) {
     // read the BPB from the disk into the supplied struct
-    Disk = open(path.c_str(), O_RDWR, 0);
+    const char* cpath = path.c_str();
+    Disk = open(cpath, O_RDWR, 0);
     lseek(Disk, 0, 0);
     if(read(Disk, (char*)&BPB, sizeof(BPB)) == -1) {
+        cprintf("Failed to read BPB");
         return false;
     }
 
     // read the File Allocation Table from the disk
+    FAT = (uint32_t*)malloc(BPB.BPB_BytsPerSec * BPB.BPB_FATSz32);
     lseek(Disk, BPB.BPB_RsvdSecCnt * BPB.BPB_BytsPerSec, 0);
     if(read(Disk, FAT, BPB.BPB_BytsPerSec * BPB.BPB_FATSz32) == -1) {
+        cprintf("Failed to read FAT");
         return false;
     }
 
